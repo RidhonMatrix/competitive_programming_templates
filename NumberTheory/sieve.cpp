664e6ff4c3ae04adcc98b06854d1ad82f5f52c9d
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2022-06-04 12:15:42 +0600

    Update sieve.cpp

--- NumberTheory/sieve.cpp
+++ NumberTheory/sieve.cpp
@@ -1,13 +1,13 @@
 //O(N log (log N))
 const int N=1e7;
 bool marked[N];
+vector<ll>p;
 bool isPrime(int n){
 	if(n<2)  return false;
 	if(n==2) return true;
 	if(n%2==0) return false;
 	return marked[n]==false; 
 }
-vector<ll>p;
 void sieve(int n){
 	for(int i=3;i*i<=n;i+=2){
 		if(marked[i]==false){
@@ -16,11 +16,12 @@ void sieve(int n){
 			}
 		}
 	}
+	for(int i=2;i<N;i++){
+    		if(isPrime(i)) p.push_back(i);
+	}
 }
 
 sieve(N);
-for(int i=2;i<N;i++){
-    if(isPrime(i)) p.push_back(i);
-}
+
 
 
