36ce9035b6956f76a1daa1e33175bf925fd102f8
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2022-11-28 15:31:40 +0600

    Update cycleDetect.cpp

--- Graph/cycleDetect.cpp
+++ Graph/cycleDetect.cpp
@@ -1,3 +1,4 @@
+///bfs
 bool detect(int v, int vis[], vector<int> adj[]){
         vis[v] = 1;
         
@@ -19,4 +20,20 @@ bool detect(int v, int vis[], vector<int> adj[]){
             }
         }
         return false;
-    }
+}
+
+//dfs
+bool dfs(int v, int vis[], vector<int> adj[], int par){
+        vis[v] = true;
+        
+        for(int i = 0; i < adj[v].size(); ++i){
+            int child = adj[v][i];
+            if(!vis[child]){
+                if(dfs(child,vis,adj,v)) return true;
+            }
+            else if(par != child){
+                return true;
+            }
+        }
+        return false;
+}
