74f12b3cab507bcfb032bd6af746943452d1f933
Rezaul Karim Ridhon <71286790+RidhonMatrix@users.noreply.github.com>
2023-03-26 20:13:26 +0600

    Update cycleDetect.cpp

--- Graph/cycleDetect.cpp
+++ Graph/cycleDetect.cpp
@@ -37,3 +37,78 @@ bool dfs(int v, int vis[], vector<int> adj[], int par){
         }
         return false;
 }
+
+//cycle in directed graph
+
+stack<int> st;
+ 
+bool cycleCheck(int v, vector<int> adj[], int vis[], int pathVis[])
+{
+    st.push(v);
+    vis[v] = 1;
+    pathVis[v] = 1;
+    for (auto child : adj[v])
+    {
+        if (!vis[child])
+        {
+            if (cycleCheck(child, adj, vis, pathVis) == true)
+                return true;
+        }
+        else if (pathVis[child])
+        {
+            st.push(child);
+            return true;
+        }
+    }
+    st.pop();
+    pathVis[v] = 0;
+    return false;
+}
+ 
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+ 
+    vector<int> adj[m + 5];
+    int vis[n + 5] = {0};
+    int pathVis[n + 5] = {0};
+ 
+    int x, y;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> x >> y;
+        adj[x].push_back(y);
+    }
+    bool path = false;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (!vis[i])
+        {
+            if (cycleCheck(i, adj, vis, pathVis) == true)
+            {
+                path = true;
+                break;
+            }
+        }
+    }
+    vector<int> ans;
+    if (path)
+    {
+        int tmp = st.top();
+        while (!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+            if (ans.back() == tmp and ans.size() != 1)
+                break;
+        }
+        reverse(ans.begin(), ans.end());
+        cout << ans.size() << endl;
+        for (int i = 0; i < ans.size(); ++i)
+            cout << ans[i] << ' ';
+        cout << endl;
+    }
+    else
+        cout << "IMPOSSIBLE" << '\n';
+}
